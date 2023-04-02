32e49a6f1d64c9d930d24d30a944ba429c6f796b
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-04-02 22:17:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,25 +30,14 @@ app.get('/', async (req, res) => {
 app.post('/createUser', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
-        if (req.body.name && req.body.email && req.body.username && req.body.password) {
-            const db = await client.db('Axios')
-            let user = await db.collection('all users').findOne({ email: req.body.email })
-            if (!user) {
-                let username = await db.collection('all users').findOne({ username: req.body.username })
-                if (!username) {
-                    await db.collection('all users').insertOne(req.body)
-                    res.status(201).send({ message: 'User Registartion Successful', data: req.body })
-                }
-                else {
-                    res.status(400).send({ message: `User with username ${req.body.username} already exist` })
-                }
-            }
-            else {
-                res.status(400).send({ message: `User with ${req.body.email} already exist` })
-            }
+        const db = await client.db('Axios')
+        let username = await db.collection('all users').findOne({ username: req.body.username })
+        if (!username) {
+            await db.collection('all users').insertOne(req.body)
+            res.status(201).send({ message: 'User Registartion Successful', data: req.body })
         }
         else {
-            res.status(400).send({ message: 'name, email, username, password are mandatory' })
+            res.status(400).send({ message: `User with username ${req.body.username} already exist` })
         }
     }
     catch (error) {
@@ -64,19 +53,14 @@ app.post('/createUser', async (req, res) => {
 app.put('/updateUser/:email', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
-        if (req.params.email) {
-            const db = await client.db('Axios')
-            let user = await db.collection('all users').findOne({ email: req.params.email })
-            if (user) {
-                let user = await db.collection('all users').updateOne({ email: req.params.email }, { $set: req.body })
-                res.status(200).send({ message: 'User info updated successfully' })
-            }
-            else {
-                res.status(400).send({ message: `User not found with email ${req.params.email}` })
-            }
+        const db = await client.db('Axios')
+        let user = await db.collection('all users').findOne({ username: req.params.username })
+        if (user) {
+            let user = await db.collection('all users').updateOne({ username: req.params.username }, { $set: req.body })
+            res.status(200).send({ message: 'User info updated successfully' })
         }
         else {
-            res.status(400).send({ message: 'email is mandatory' })
+            res.status(400).send({ message: `User not found with username ${req.params.username}` })
         }
     }
     catch (error) {
@@ -91,18 +75,13 @@ app.put('/updateUser/:email', async (req, res) => {
 app.get('/userLogin/:username/:password', async (req, res) => {
     const client = await MongoClient.connect(dbUrl)
     try {
-        if (req.params.username && req.params.password) {
-            const db = await client.db('Axios')
-            let user = await db.collection('all users').findOne({ username: req.params.username, password: req.params.password })
-            if (user) {
-                res.status(200).send({ message: 'Login Successful', data: user })
-            }
-            else {
-                res.status(400).send({ message: `User not found with username ${req.params.username} and password ${req.params.password}` })
-            }
+        const db = await client.db('Axios')
+        let user = await db.collection('all users').findOne({ username: req.params.username, password: req.params.password })
+        if (user) {
+            res.status(200).send({ message: 'Login Successful', data: user })
         }
         else {
-            res.status(400).send({ message: 'email and password are mandatory' })
+            res.status(400).send({ message: `User not found with username ${req.params.username} and password ${req.params.password}` })
         }
     }
     catch (error) {
